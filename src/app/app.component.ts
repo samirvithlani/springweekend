b19ce7127f535487e75cb87f1f9d4e9d21307b7d
=samirvithlani <=samir.vithlani83955@gmail.com>
2020-09-30 11:16:51 +0530

    first lec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'sundayAngular';
+  name: string = "samir";
+  age: number = 20;
+  isActive: boolean = true;
+
+  birthDate: any = "1995-02-14";
+
+  employees: Array<any> = [
+    "jay", "raj", "parth", "hem", "meet"
+  ];
+
+  students: Array<any> = [
+
+    { name: "samir", age: 20 },
+    { name: "raj", age: 22 },
+    { name: "parth", age: 24 },
+    { name: "hem", age: 19 }
+
+  ]
+
+
 }
